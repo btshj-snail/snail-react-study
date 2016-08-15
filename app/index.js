8018947be3034f494097531ae916474bc569f505
snail <snail>
2016-08-15 16:24:00 +0800

    s

--- app/index.js
+++ app/index.js
@@ -22,6 +22,7 @@ let app  = document.createElement('div');
 const myPromise = Promise.resolve(42);
 myPromise.then((number) => {
     $('body').append('<p>promise result is ' + number + ' now is ' + moment().format() + '</p>');
+//    
 });
 app.innerHTML = '<h1>Hello World it</h1>';
 document.body.appendChild(app);
